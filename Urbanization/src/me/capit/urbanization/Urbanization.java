05de3125b6489811bff160b382d5750acb3b12be
NukesForKids <matt@capit.me>
2014-12-14 08:34:29 -0800

    Created the Urbanization project.

---
+++ Urbanization/src/me/capit/urbanization/Urbanization.java
@@ -0,0 +1,24 @@
+package me.capit.urbanization;
+
+import me.capit.entropy.module.EntropyModule;
+import me.capit.entropy.module.ModuleMeta;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Urbanization extends EntropyModule {
+	private static final long serialVersionUID = -7024584539096221029L;
+
+	public Urbanization(JavaPlugin plugin, ModuleMeta meta) {
+		super(plugin, meta);
+	}
+	
+	@Override
+	protected void onEnable() {
+		
+	}
+	
+	@Override
+	protected void onDisable() {
+		
+	}
+}
